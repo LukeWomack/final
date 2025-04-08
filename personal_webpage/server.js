b90c0735851941b70b4cfe88df4373740aed2e3f
LukeWomack <lwomack2@atu.edu>
2025-04-08 13:54:35 -0500

    basic server.js config

--- personal_webpage/server.js
+++ personal_webpage/server.js
@@ -1,4 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const dotenv = require()
+const dotenv = require('dotenv');
+const indexRoutes = require('/routes/index');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.port || 3000;
+
+//DB config
+require('./config/db')();
+
+//middleware
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// importing routes
+app.use('/', indexRoutes);
+
+//start server
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
