7f17e1d8ff5f60aac3fb0ac267cd1a05394bf6ba
LukeWomack <lwomack2@atu.edu>
2025-04-08 16:10:25 -0500

    fixed mainController import

--- personal_webpage/routes/index.js
+++ personal_webpage/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const mainController = require('...controllers/mainController');
+const mainController = require('../controllers/mainController.js');
 
 router.get('/', mainController.home);
 router.get('/contact', mainController.contact);
